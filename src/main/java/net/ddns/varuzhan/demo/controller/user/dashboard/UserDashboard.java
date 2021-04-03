accaef03d4db61d730441ca347052745e47097f1
Varuzh <varuzhanst@gmail.com>
2021-04-03 19:00:25 +0400

    stepup-5 Assignment visibilty for users

--- src/main/java/net/ddns/varuzhan/demo/controller/user/dashboard/UserDashboard.java
+++ src/main/java/net/ddns/varuzhan/demo/controller/user/dashboard/UserDashboard.java
@@ -2,10 +2,7 @@ package net.ddns.varuzhan.demo.controller.user.dashboard;
 
 import net.ddns.varuzhan.demo.dto.SubjectsFilterDto;
 import net.ddns.varuzhan.demo.dto.SubjectsIDsForFilterDto;
-import net.ddns.varuzhan.demo.model.ClassMaterial;
-import net.ddns.varuzhan.demo.model.ManagersGroupsSubjects;
-import net.ddns.varuzhan.demo.model.SubjectInfo;
-import net.ddns.varuzhan.demo.model.UserGroupInfo;
+import net.ddns.varuzhan.demo.model.*;
 import net.ddns.varuzhan.demo.service.prototype.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -25,12 +22,14 @@ public class UserDashboard {
     private final UserGroupInfoService userGroupInfoService;
     private final ManagersGroupsSubjectsService managersGroupsSubjectsService;
     private final ClassMaterialService classMaterialService;
+    private final AssignmentService assignmentService;
     private final SubjectInfoService subjectInfoService;
-    public UserDashboard(UserService userService, UserGroupInfoService userGroupInfoService, ManagersGroupsSubjectsService managersGroupsSubjectsService, ClassMaterialService classMaterialService, SubjectInfoService subjectInfoService) {
+    public UserDashboard(UserService userService, UserGroupInfoService userGroupInfoService, ManagersGroupsSubjectsService managersGroupsSubjectsService, ClassMaterialService classMaterialService, AssignmentService assignmentService, SubjectInfoService subjectInfoService) {
         this.userService = userService;
         this.userGroupInfoService = userGroupInfoService;
         this.managersGroupsSubjectsService = managersGroupsSubjectsService;
         this.classMaterialService = classMaterialService;
+        this.assignmentService = assignmentService;
         this.subjectInfoService = subjectInfoService;
     }
 
@@ -44,11 +43,14 @@ public class UserDashboard {
         if(groupInfoByUser==null) return "redirect:/login?noGroup";
         HashSet<ManagersGroupsSubjects> userAvailableManagersSubjects= (HashSet<ManagersGroupsSubjects>) managersGroupsSubjectsService.getManagerSubjectByGroup(groupInfoByUser.getGroupInfo());
         TreeSet<ClassMaterial> userClassMaterials = new TreeSet<>();
+        TreeSet<Assignment> userAssignments = new TreeSet<>();
         TreeSet<SubjectsIDsForFilterDto> userAvailableSubjects = new TreeSet<>();
         userAvailableSubjects.add(new SubjectsIDsForFilterDto("-1","Բոլոր առարկաները"));
         for(ManagersGroupsSubjects x : userAvailableManagersSubjects){
             Set<ClassMaterial> setOfMaterials = classMaterialService.getMaterialsByManagerGroupSubject(x);
+            Set<Assignment> setOfAssignments = assignmentService.getAssignmentsByManagerGroupSubject(x);
             userClassMaterials.addAll(setOfMaterials);
+            userAssignments.addAll(setOfAssignments);
             userAvailableSubjects.add(new SubjectsIDsForFilterDto(x.getSubjectInfo().getId().toString(),x.getSubjectInfo().getSubjectName()));
         }
 
@@ -56,12 +58,14 @@ public class UserDashboard {
             subjectsFilterDto.setSubjectId("-1");
             model.addAttribute("userMaterials",userClassMaterials);
             model.addAttribute("availSubjects",userAvailableSubjects);
+            model.addAttribute("userAssignments",userAssignments);
             model.addAttribute("subjectFilter",subjectsFilterDto);
         }
         else{
             userClassMaterials.clear();
             for(ManagersGroupsSubjects x : userAvailableManagersSubjects){
-
+                Set<Assignment> setOfAssignments = assignmentService.getAssignmentsByManagerGroupSubject(x);
+                userAssignments.addAll(setOfAssignments);
                 if(x.getSubjectInfo().equals(subjectInfoService.getSubjectInfoById(subjectsFilterDto.getSubjectId()))){
                     Set<ClassMaterial> setOfMaterials = classMaterialService.getMaterialsByManagerGroupSubject(x);
                     userClassMaterials.addAll(setOfMaterials);
@@ -70,13 +74,11 @@ public class UserDashboard {
                 model.addAttribute("userMaterials",userClassMaterials);
                 model.addAttribute("availSubjects",userAvailableSubjects);
                 model.addAttribute("subjectFilter",subjectsFilterDto);
+                model.addAttribute("userAssignments",userAssignments);
             }
         }
         String groupNumber = " (" + groupInfoByUser.getGroupInfo().getGroupNumber() + ")";
         model.addAttribute("full_name_group_number", fullName + groupNumber );
-
-
-
         return "user/dashboard/userDashboard";
     }
 
